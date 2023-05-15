455ddcac6c2a44db9745a8466fe0ab56b72799f1
CC25MC <cesarmiguel199@gmail.com>
2023-05-14 20:33:20 -0400

    wip

--- Servidor-Express/database/db.js
+++ Servidor-Express/database/db.js
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
-const db = new Sequelize("ProyectoDesarrolloWeb", "postgres", "1234", {
-  host: "localhost",
+const db = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
   dialect: "postgres",
 });
 
--- Servidor-Express/index.js
+++ Servidor-Express/index.js
@@ -1,4 +1,4 @@
-import express  from 'express'
+import express from 'express'
 import cors from 'cors'
 import db from './database/db.js'
 import authRoutes from "./routes/auth.route.js";
