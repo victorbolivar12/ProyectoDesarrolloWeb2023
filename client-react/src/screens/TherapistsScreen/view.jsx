9d30fd5498a3c2887a704f8475fabd4a37301e09
antonio13castillo93 <132378910+antonio13castillo93@users.noreply.github.com>
2023-07-23 16:32:54 -0400

    Update view.jsx
    xs

--- client-react/src/screens/TherapistsScreen/view.jsx
+++ client-react/src/screens/TherapistsScreen/view.jsx
@@ -3,7 +3,6 @@ import { DataGrid } from "@mui/x-data-grid";
 import { Box, Typography, IconButton ,Button} from "@mui/material";
 import { Delete, Edit } from "@mui/icons-material";
 
-import * as React from 'react';
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
@@ -12,44 +11,6 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
-  },
-  '& .MuiDialogActions-root': {
-    padding: theme.spacing(1),
-  },
-}));
-
-function BootstrapDialogTitle(props) {
-  const { children, onClose, ...other } = props;
-
-  return (
-    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-      {children}
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </DialogTitle>
-  );
-}
-
-BootstrapDialogTitle.propTypes = {
-  children: PropTypes.node,
-  onClose: PropTypes.func.isRequired,
-};
 
 const columns = [
   { field: "id", headerName: "Id", width: 250 },
@@ -88,7 +49,7 @@ const columns = [
       };
 
       const handleClickOpen = () => {
-          setOpen(true);
+          return alert("Action");
       };
 
       return (
