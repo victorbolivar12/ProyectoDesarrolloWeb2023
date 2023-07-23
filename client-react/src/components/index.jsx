10d53aab62c26ce48626519e9f6506c343b882d6
antonio13castillo93 <132378910+antonio13castillo93@users.noreply.github.com>
2023-07-23 18:25:10 -0400

    Update index.jsx

--- client-react/src/components/index.jsx
+++ client-react/src/components/index.jsx
@@ -1,7 +1,6 @@
 export * from "./Loader";
 export * from "./TextInput";
 export * from "./Button";
-export * from "./Dialogs";
 export * from "./Link";
 export * from "./Sidebar";
 export * from "./AppBar";
