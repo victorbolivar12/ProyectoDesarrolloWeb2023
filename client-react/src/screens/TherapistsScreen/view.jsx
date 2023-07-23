9e72c366454da45f0ce03007341d876d0f8a866a
antonio13castillo93 <132378910+antonio13castillo93@users.noreply.github.com>
2023-07-23 16:29:27 -0400

    Update view.jsx

--- client-react/src/screens/TherapistsScreen/view.jsx
+++ client-react/src/screens/TherapistsScreen/view.jsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { Box, Typography, IconButton } from "@mui/material";
+import { Box, Typography, IconButton ,Button} from "@mui/material";
 import { Delete, Edit } from "@mui/icons-material";
 
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -13,7 +12,6 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
