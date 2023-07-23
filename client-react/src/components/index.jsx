51c6e3d2251b23413242c5bca442bc7c2b371bd0
antonio13castillo93 <132378910+antonio13castillo93@users.noreply.github.com>
2023-07-23 17:10:50 -0400

    Update index.jsx
    agregando componente Dialogs

--- client-react/src/components/index.jsx
+++ client-react/src/components/index.jsx
@@ -1,6 +1,7 @@
 export * from "./Loader";
 export * from "./TextInput";
 export * from "./Button";
+export * from "./Dialogs";
 export * from "./Link";
 export * from "./Sidebar";
 export * from "./AppBar";
